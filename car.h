437e34e8713195ce083e772ef2585c6e236a2232
anthropomorphic <code@mikefdorst.com>
2014-07-15 15:56:27 -0700

    Remove comment, add setters

--- car.h
+++ car.h
@@ -12,22 +12,12 @@
 #include <string>
 
 class car {
-    /**
-     * unsigned short sequence
-     **
-     * The above is unnecessary for this application,
-     * as a std::deque keeps track of the order
-     * of its elements for you.
-     * Also, the maximum value of an unsigned short on my system is 65535.
-     * That might well be insufficient in an extreme case; certainly a
-     * std::deque can hold many more elements than that.
-     * Perhaps size_t, uint64_t, or simply unsigned long would be a better choice.
-     */
     double amount_paid;
     std::string description;
-    
 public:
-    
+    void set_amount_paid(double ap)       { amount_paid = ap; }
+    void set_description(std::string& d)  { description = d;  }
+    void set_description(std::string&& d) { description = d;  }
 };
 
 #endif
