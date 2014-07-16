2316766e41d3ff2188ebe977973ed27dd4c68c39
anthropomorphic <code@mikefdorst.com>
2014-07-16 01:22:21 -0700

    Remove unused setters from car.
    Delete default constructor on car.
    Implement move semantics on car constructor.

--- car.h
+++ car.h
@@ -14,19 +14,18 @@
 class car {
     std::string description;
     double amount_paid;
-public:
-    void set_description(std::string& d)  { description = d;  }
-    void set_description(std::string&& d) { description = d;  }
+    
     void set_amount_paid(double ap) {
         if (ap >= 0) {
             amount_paid = ap;
         }
     }
-    car() = default;
+public:
+    car() = delete;
     car(std::string& d, double ap) : description(d) {
         set_amount_paid(ap);
     }
-    car(std::string&& d, double ap) : description(d) {
+    car(std::string&& d, double ap) : description(std::move(d)) {
         set_amount_paid(ap);
     }
     friend std::ostream& operator << (std::ostream&, car&);
