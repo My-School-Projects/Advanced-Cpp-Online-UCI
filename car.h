ea642bbd49c3f09848624a8e2a9555f03d2f5400
anthropomorphic <code@mikefdorst.com>
2014-07-20 04:16:58 -0700

    Add new data to car

--- car.h
+++ car.h
@@ -10,15 +10,18 @@
 #define Parking_Lot__car_h
 
 #include <string>
+#include <ctime>
 
 class car {
+    std::string plate_number;
     std::string description;
+    std::time_t time_in;
     
 public:
     car() = delete;
-    car(std::string&& d) : description(std::move(d)) {
-        
-    }
+    car(std::string&& pn, std::time_t t, std::string&& d) :
+    plate_number(pn), time_in(t), description(std::move(d)) {}
+    
     friend std::ostream& operator << (std::ostream&, car&);
 };
 
