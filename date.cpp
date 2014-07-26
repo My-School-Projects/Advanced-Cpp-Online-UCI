cab4fe5a10c18fae3bde6818dc0a59d476807826
anthropomorphic <code@mikefdorst.com>
2014-07-25 18:36:52 -0700

    Add date.cpp

---
+++ date.cpp
@@ -0,0 +1,33 @@
+/**
+ *  Michael Dorst
+ *  Assignment 3
+ *  Used Car Dealership
+ *
+ *  date.h
+ */
+
+#include "date.h"
+#include <ciso646>
+
+
+std::ostream& operator << (std::ostream& out, const date_t& d) {
+    return out<<d._month<<"/"<<d._day<<"/"<<d._year;
+}
+
+std::istream& operator >> (std::istream& in, date_t& d) {
+    using std::tie;
+    std::string input;
+    uint16_t month, day, year;
+    std::getline(in, input, '/');
+    month = (uint16_t)atoi(input.c_str());
+    std::getline(in, input, '/');
+    day = (uint16_t)atoi(input.c_str());
+    std::getline(in, input, '\n');
+    year = (uint16_t)atoi(input.c_str());
+    
+    d = date_t(month, day, year);
+    if (d.month() != month or d.day() != day or d.year() != year) {
+        in.setstate(std::ios::failbit);
+    }
+    return in;
+}
--- date.h
+++ date.h
@@ -60,26 +60,4 @@ public:
     friend std::istream& operator >> (std::istream&, date_t&);
 };
 
-std::ostream& operator << (std::ostream& out, const date_t& d) {
-    return out<<d._month<<"/"<<d._day<<"/"<<d._year;
-}
-
-std::istream& operator >> (std::istream& in, date_t& d) {
-    using std::tie;
-    std::string input;
-    uint16_t month, day, year;
-    std::getline(in, input, '/');
-    month = (uint16_t)atoi(input.c_str());
-    std::getline(in, input, '/');
-    day = (uint16_t)atoi(input.c_str());
-    std::getline(in, input, '\n');
-    year = (uint16_t)atoi(input.c_str());
-    
-    d = date_t(month, day, year);
-    if (d.month() != month or d.day() != day or d.year() != year) {
-        in.setstate(std::ios::failbit);
-    }
-    return in;
-}
-
 #endif
