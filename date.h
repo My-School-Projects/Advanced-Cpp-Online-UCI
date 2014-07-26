5e2f7c190dfb8fc78e17d6f07b803690f61299a2
anthropomorphic <code@mikefdorst.com>
2014-07-25 23:38:48 -0700

    Move date ctor and getter definitions to date.cpp

--- date.h
+++ date.h
@@ -20,41 +20,10 @@ class date_t {
     
 public:
     
-    date_t(uint16_t m = 1, uint16_t d = 1, uint16_t y = 2000) : _year(y) {
-        if (m > 0 and m <= 12) {
-            _month = m;
-        } else {
-            _month = 1;
-        }
-        uint16_t max_days;
-        switch (_month) {
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                max_days = 30;
-                break;
-            case 2:
-                if (_year % 4) {
-                    max_days = 29;
-                } else {
-                    max_days = 28;
-                }
-                break;
-            default:
-                max_days = 31;
-                break;
-        }
-        if (d > 0 and d <= max_days) {
-            _day = d;
-        } else {
-            _day = 1;
-        }
-    }
-    
-    uint16_t month()const { return _month; }
-    uint16_t day()const { return _day; }
-    uint16_t year()const { return _year; }
+    date_t(uint16_t m = 1, uint16_t d = 1, uint16_t y = 2000);
+    uint16_t month()const;
+    uint16_t day()const;
+    uint16_t year()const;
     
     friend std::istream& operator >> (std::istream&, date_t&);
 };
