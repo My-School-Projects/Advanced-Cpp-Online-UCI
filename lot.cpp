1a553ba6a1098ddd73c43e6cb03f12512da55765
anthropomorphic <code@mikefdorst.com>
2014-07-27 16:33:43 -0700

    Mark lot_t::begin_by_*() / end_by_*() const

--- lot.cpp
+++ lot.cpp
@@ -27,18 +27,18 @@ bool lot_t::insert(car_t car) {
     return success;
 }
 
-std::map<car_t::inv_num_t, car_t>::const_iterator lot_t::begin_by_inv_num() {
-    return by_inv_num.begin();
+std::map<car_t::inv_num_t, car_t>::const_iterator lot_t::begin_by_inv_num()const {
+    return by_inv_num.cbegin();
 }
 
-std::map<car_t::inv_num_t, car_t>::const_iterator lot_t::end_by_inv_num() {
-    return by_inv_num.end();
+std::map<car_t::inv_num_t, car_t>::const_iterator lot_t::end_by_inv_num()const {
+    return by_inv_num.cend();
 }
 
-std::multimap<date_t, car_t>::const_iterator lot_t::begin_by_date() {
-    return by_date.begin();
+std::multimap<date_t, car_t>::const_iterator lot_t::begin_by_date()const {
+    return by_date.cbegin();
 }
 
-std::multimap<date_t, car_t>::const_iterator lot_t::end_by_date() {
-    return by_date.end();
+std::multimap<date_t, car_t>::const_iterator lot_t::end_by_date()const {
+    return by_date.cend();
 }
