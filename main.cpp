54bc327d25bddf48bcd854195744001242a27450
anthropomorphic <code@mikefdorst.com>
2014-08-04 23:08:14 -0700

    Add generateAccounts()

--- main.cpp
+++ main.cpp
@@ -7,13 +7,32 @@
  */
 
 #include "account.h"
-#include <ostream>
 #include <iostream>
 #include <iomanip>
+#include <vector>
 #include <ciso646>
+using std::vector;
+using std::string;
+using std::to_string;
+using std::cout;
+using std::endl;
+
+vector<Account> generateAccounts(size_t, string);
 
 int main() {
     
+    auto v = generateAccounts(5, "Client A");
+    for (auto i : v) {
+        cout<<i<<endl;
+    }
     
-    
+}
+
+vector<Account> generateAccounts(size_t n, string baseName) {
+    vector<Account> vec;
+    vec.reserve(n);
+    for (size_t i = 1; i <= n; i++) {
+        vec.push_back(Account(baseName+to_string(i), arc4random() % 1000));
+    }
+    return vec;
 }
